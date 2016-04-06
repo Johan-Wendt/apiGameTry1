610430321db5f3a236ea72ac1108ec2df1a824f1
Johan Wendt <johan.wendt@telavox.se>
2016-04-06 07:52:07 +0200

    ff

--- src/main/java/rest/Player.java
+++ src/main/java/rest/Player.java
@@ -5,4 +5,16 @@ public class Player extends Snake {
 	public Player(MasterController masterController, byte playerNumber) {
 		super(masterController, playerNumber);
 	}
+
+	@Override
+	public void setObjectType() {
+		super.setObjectTypeNumber(ObjectTypes.PLAYER);
+		
+	}
+
+	@Override
+	public void setObjectNumber() {
+		// TODO Auto-generated method stub
+		
+	}
 }
